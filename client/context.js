281720253aa1e56f41a599c56d25d100d168fd2d
kaseywolff <kaseywolff5@gmail.com>
2023-05-23 10:26:42 -0700

    work with mark on setup

---
+++ client/context.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+// export const userContext = createContext({ user: null, setUser: () => { } });
+export const userContext = createContext({ user: 'Kasey', setUser: () => { } });
